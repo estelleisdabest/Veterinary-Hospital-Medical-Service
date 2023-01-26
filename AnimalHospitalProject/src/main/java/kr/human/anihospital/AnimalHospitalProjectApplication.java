99ba9327fbe3a5796c23fc038b23135c8b4f9129
jyjy1007 <117172261+jyjy1007@users.noreply.github.com>
2023-01-26 16:13:55 +0900

    create Veterinary Hospital Medical Service project

---
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/AnimalHospitalProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.human.anihospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHospitalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalHospitalProjectApplication.class, args);
+	}
+
+}
---
+++ AnimalHospitalProject/src/test/java/kr/human/anihospital/AnimalHospitalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.human.anihospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHospitalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
