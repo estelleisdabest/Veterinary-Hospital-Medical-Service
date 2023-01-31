cf7166fac48bb8bf754f1faf3029347f3be1457e
Correctchain <122464707+Correctchain@users.noreply.github.com>
2023-01-31 14:20:05 +0900

    후기작성화면 추가

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/MainController.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/MainController.java
@@ -15,4 +15,12 @@ public class MainController {
 	public String signin() {
 		return "signin";
 	}
+	@GetMapping("/postscript_save")
+	public String postscript_save() {
+		return "postscript_save";
+	}
+	@GetMapping("/postscript_update")
+	public String postscript_update() {
+		return "postscript_update";
+	}
 }
