a15f9a44457e754fbe96e1cea3e64aa8b81399d4
배지현 <mkiopl01@gmail.com>
2023-02-01 11:27:17 +0900

    MainController 수정

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/MainController.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/MainController.java
@@ -29,7 +29,8 @@ public class MainController {
 	@GetMapping("/diagnosis")
 	public String diagnosis() {
 		return "diagnosis";
-
+	}
+	
 	@GetMapping("/roleCheck")
 	public String roleCheck() {
 		return "roleCheck";
