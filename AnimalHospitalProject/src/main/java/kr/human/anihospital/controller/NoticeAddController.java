29cc836bb4359b5fed68e3c3c90d01a77805c62d
miko1220 <human@DESKTOP-UORS0GS>
2023-02-16 12:25:03 +0900

    공지화면 CRUD 추가 및 서비스 임플 삭제

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/NoticeAddController.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/NoticeAddController.java
@@ -3,32 +3,33 @@ package kr.human.anihospital.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.human.anihospital.service.NoticeAddService;
 import kr.human.anihospital.vo.NoticeVO;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 @Slf4j
 public class NoticeAddController {
 	@Autowired
 	NoticeAddService noticeAddService;
 	
-	@PostMapping("/noticeAdd")
+	// 나중에 seq 처리 해줘야함
+	@PostMapping("/noticeAddOk")
 	public String insertNotice(@RequestParam Map<String, String> noticeAddMap, Model model) throws Exception{
 		log.info("받은 값 : {}", noticeAddMap);
 		NoticeVO noticeVO = new NoticeVO();
 		noticeVO.setSeqDoctor(1);
 		noticeVO.setSeqAnimalHospital(1);
-		noticeVO.setNoticeSubject(noticeAddMap.get("noticeSubject"));
-		noticeVO.setNoticeContent(noticeAddMap.get("noticeContent"));
+		noticeVO.setNoticeSubject(noticeAddMap.get("noticeAddSubject"));
+		noticeVO.setNoticeContent(noticeAddMap.get("noticeAddContent"));
 		
-		noticeAddService.insertNoticeInfo(noticeVO);
+		noticeAddService.insertNotice(noticeVO);
 		
-		return "noticeDetail";
+		return "성공";
 	}
 }
--- AnimalHospitalProject/src/main/java/kr/human/anihospital/service/NoticeAddService.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/service/NoticeAddService.java
@@ -1,7 +1,25 @@
 package kr.human.anihospital.service;
 
-import kr.human.anihospital.vo.NoticeVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface NoticeAddService {
-	void insertNoticeInfo (NoticeVO noticeVO) throws Exception;
+import kr.human.anihospital.mapper.NoticeAddMapper;
+import kr.human.anihospital.vo.NoticeVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class NoticeAddService {
+	
+	@Autowired
+	NoticeAddMapper noticeAddMapper;
+	
+	public void insertNotice (NoticeVO noticeVO) {
+		try {
+			log.info("서비스 받은 값 : {}", noticeVO);
+			noticeAddMapper.insertNotice(noticeVO);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
