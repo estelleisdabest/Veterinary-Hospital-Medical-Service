089f41775bcc4e3248385039a9052a2652ea7ffa
Correctchain <122464707+Correctchain@users.noreply.github.com>
2023-02-16 16:58:20 +0900

    후기화면 crud,ajax 수정

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/PostscriptSaveController.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/PostscriptSaveController.java
@@ -20,7 +20,7 @@ public class PostscriptSaveController {
 	@Autowired
 	PostscriptSaveService postscriptSaveService;
 	
-	@PostMapping("/diagnosis")
+	@PostMapping("/postscriptSave")
 	public String insertPostscript(@RequestParam Map<String, String> postscriptMap, Model model) throws Exception{
 		log.info("화면에서 넘어온 값1(컨트롤러) : {}",postscriptMap);
 		PostscriptVO postscriptVO = new PostscriptVO();
@@ -34,7 +34,7 @@ public class PostscriptSaveController {
 		postscriptSaveService.insertPostscriptInfo(postscriptVO);
 		
 		log.info("서비스에서 넘어온 값2(컨트롤러) : {}",postscriptMap);
-		return "성공";
+		return "diagnosis";
 		
 		
 	}
