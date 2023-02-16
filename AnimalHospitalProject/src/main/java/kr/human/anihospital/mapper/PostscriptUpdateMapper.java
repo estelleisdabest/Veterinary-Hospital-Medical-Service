608de59226628b006e635b8505ed8ad9f2102bc8
Correctchain <122464707+Correctchain@users.noreply.github.com>
2023-02-16 16:02:25 +0900

    후기수정crud작성,js수정,ajax추가

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/PostscriptUpdateMapper.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/PostscriptUpdateMapper.java
@@ -1,5 +1,12 @@
 package kr.human.anihospital.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+
+import kr.human.anihospital.vo.PostscriptVO;
+
+@Mapper
 public interface PostscriptUpdateMapper {
 
+	void updatePostscript(PostscriptVO postscriptVO) throws Exception;
 }
