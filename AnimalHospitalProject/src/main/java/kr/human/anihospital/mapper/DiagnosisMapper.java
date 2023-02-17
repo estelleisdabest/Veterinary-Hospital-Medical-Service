34d19203af5825eaae657b9eee68346098b43ad7
Correctchain <122464707+Correctchain@users.noreply.github.com>
2023-02-17 18:40:00 +0900

    진료내역 화면 수정 및 crud추가,mapper수정

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/DiagnosisMapper.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/DiagnosisMapper.java
@@ -6,9 +6,11 @@ import org.apache.ibatis.annotations.Mapper;
 
 import kr.human.anihospital.vo.DiagnosisAnimalVO;
 import kr.human.anihospital.vo.DiagnosisDetailInMedicineVO;
+import kr.human.anihospital.vo.postscriptOneDiagnosisVO;
 
 @Mapper
 public interface DiagnosisMapper {
 	DiagnosisAnimalVO selectDiagnosisAnimalInfo(int seqDignosis) throws Exception;
 	List<DiagnosisDetailInMedicineVO> selectDiagnosisDetailInMedicine() throws Exception;
+	postscriptOneDiagnosisVO selectPostscriptOneDiagnosis() throws Exception;
 }
