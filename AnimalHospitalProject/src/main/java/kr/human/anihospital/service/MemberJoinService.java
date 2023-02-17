55751963c76bb6f2a18f59f56ab5fdf0f56442d5
GeonWoo Yun <kwyoon0705@gmail.com>
2023-02-17 17:15:09 +0900

    보호자 회원가입 페이지 수정 / 의사 회원가입 CRUD 구현

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/service/MemberJoinService.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/service/MemberJoinService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.human.anihospital.mapper.MemberJoinMapper;
+import kr.human.anihospital.vo.DoctorVO;
 import kr.human.anihospital.vo.ProtectorVO;
 
 @Service
@@ -32,6 +33,33 @@ public class MemberJoinService {
 			protectorVO.setProtectorAddress(String.valueOf(map.get("protector_address")));
 			protectorVO.setProtectorPhoneNo(String.valueOf(map.get("protector_phoneNumber")));
 			memberJoinMapper.protectorMemberJoin(protectorVO);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doctorMemberJoin(Map<String, Object> map) {
+		DoctorVO doctorVO = new DoctorVO();
+		try {
+			doctorVO.setDoctorId(String.valueOf(map.get("email")));
+			doctorVO.setDoctorPassword(String.valueOf(map.get("password")));
+			doctorVO.setDoctorName(String.valueOf(map.get("doctor_id")));
+			doctorVO.setDoctorGender(Integer.parseInt(String.valueOf(map.get("doctor_gender"))) == 1 ? true : false);
+			String dateString = map.get("doctor_year") + "-" + map.get("doctor_month") + "-" + map.get("doctor_date");
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Date birthDate = simpleDateFormat.parse(dateString);
+			doctorVO.setDoctorAge(birthDate);
+			String licenseString = map.get("doctor_license_year") + "-" + map.get("doctor_license_month") + "-"
+					+ map.get("doctor_license_date");
+			Date licenseDate = simpleDateFormat.parse(licenseString);
+			doctorVO.setDoctorWorkYear(licenseDate);
+			doctorVO.setDoctorRegistrationNumber(Integer.parseInt(String.valueOf(map.get("doctor_license_seq"))));
+			doctorVO.setDoctorAddress(map.get("doctor_address") + " " + map.get("address2"));
+			doctorVO.setDoctorPhoneNo(String.valueOf(map.get("doctor_phoneNumber")));
+			doctorVO.setDoctorSay(String.valueOf(map.get("greetingMessage")));
+			doctorVO.setDoctorWorkSpace(String.valueOf(map.get("careerMessage")));
+			doctorVO.setDoctorEducation(String.valueOf(map.get("educationMessage")));
+			memberJoinMapper.doctorMemberJoin(doctorVO);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
