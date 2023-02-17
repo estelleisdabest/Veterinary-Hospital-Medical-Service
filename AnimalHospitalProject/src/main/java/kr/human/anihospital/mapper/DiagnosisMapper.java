33d061a4162bd9d9a55f03f39831ff00774f5e07
배지현 <mkiopl01@gmail.com>
2023-02-17 16:48:25 +0900

    diagnosis 상세 진료 내역 select 작성

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/DiagnosisMapper.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/mapper/DiagnosisMapper.java
@@ -1,13 +1,14 @@
 package kr.human.anihospital.mapper;
 
-import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.human.anihospital.vo.DiagnosisAnimalVO;
+import kr.human.anihospital.vo.DiagnosisDetailInMedicineVO;
 
 @Mapper
 public interface DiagnosisMapper {
-	
-	DiagnosisAnimalVO selectDiagnosisAnimalInfo(int seqDignosis) throws SQLException;
+	DiagnosisAnimalVO selectDiagnosisAnimalInfo(int seqDignosis) throws Exception;
+	List<DiagnosisDetailInMedicineVO> selectDiagnosisDetailInMedicine() throws Exception;
 }
--- AnimalHospitalProject/src/main/java/kr/human/anihospital/service/DiagnosisService.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/service/DiagnosisService.java
@@ -1,7 +1,13 @@
 package kr.human.anihospital.service;
 
+import java.util.List;
+
 import kr.human.anihospital.vo.DiagnosisAnimalVO;
+import kr.human.anihospital.vo.DiagnosisDetailInMedicineVO;
+import kr.human.anihospital.vo.DiagnosisVO;
+import kr.human.anihospital.vo.MedicineVO;
 
 public interface DiagnosisService {
 	DiagnosisAnimalVO selectDiagnosisAnimalInfo(int seqDiagnosis) throws Exception;
+	List<DiagnosisDetailInMedicineVO> selectDiagnosisDetailInMedicine() throws Exception;
 }
