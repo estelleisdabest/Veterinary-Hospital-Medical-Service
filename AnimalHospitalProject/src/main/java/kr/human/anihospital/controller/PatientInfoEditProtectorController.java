9735c8ec6630dc61225331f6eda2c03c47ae13fd
gpxu <gpxuss@gmail.com>
2023-02-20 16:09:13 +0900

    환자 정보 수정,등록 소스파일 통합

--- AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/PatientInfoEditProtectorController.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/controller/PatientInfoEditProtectorController.java
@@ -1,5 +1,6 @@
 package kr.human.anihospital.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,9 @@ public class PatientInfoEditProtectorController {
 	@Autowired
 	PatientInfoEditProtectorService patientInfoEditProtectorService;
 	
-	// 여기서 정보 조회화면에서 seq 받아와야함
+	//----------------------------------------------------------------------------------------------------
+	// 환자 정보 수정시 보여줄 정보 조회 메서드
+	//----------------------------------------------------------------------------------------------------
 	@GetMapping("patientInfoEditProtector")
 	public String PatientInfoEditProtectorSelect(Model model) {
 		AnimalVO animalVO = new AnimalVO();
@@ -30,10 +33,36 @@ public class PatientInfoEditProtectorController {
 		return "patientInfoEditProtector";
 	}
 	
+	//----------------------------------------------------------------------------------------------------
+	// 환자 정보 수정 후 확인 눌렀을때 수정 처리해줄 메서드
+	//----------------------------------------------------------------------------------------------------	
 	@PostMapping("patientInfoEditProtectorOk")
 	public String PatientInfoEditProtectorOk(@RequestParam Map<String, Object> map) {
 		log.info("수정값 : {}", map);
 		patientInfoEditProtectorService.updatePatient(map);
 		return "redirect:myPageProtector";
+	}
+	
+	//----------------------------------------------------------------------------------------------------
+	// 환자 정보 추가 메서드 -- seq 작업 필요
+	//----------------------------------------------------------------------------------------------------
+	@PostMapping("/patientAdd")
+	public String insertPatient(@RequestParam Map<String, String> patientAddMap, Model model ) throws Exception{
+		log.info("받은 값 : {}", patientAddMap);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		AnimalVO animalVO = new AnimalVO();
+		animalVO.setSeqProtector(1); // 추후 session값에서 가져오기
+		animalVO.setSeqDoctor(1); // seqDoctor 추후 삭제
+		animalVO.setAnimalName(patientAddMap.get("animalName"));
+		animalVO.setAnimalType(patientAddMap.get("animalType"));
+		animalVO.setAnimalSize(patientAddMap.get("animalSize"));
+		animalVO.setAnimalWeight(Float.parseFloat(patientAddMap.get("animalWeight")));
+		animalVO.setAnimalAge(sdf.parse(patientAddMap.get("animalAge")));
+		animalVO.setAnimalGender(Boolean.parseBoolean(patientAddMap.get("animalGender")));
+		animalVO.setAnimalImportantSymptom(patientAddMap.get("animalImportantSymptom"));
+		
+		patientInfoEditProtectorService.insertPatient(animalVO);
+		
+		return "animallookup";
 	}
 }
--- AnimalHospitalProject/src/main/java/kr/human/anihospital/service/PatientInfoEditProtectorService.java
+++ AnimalHospitalProject/src/main/java/kr/human/anihospital/service/PatientInfoEditProtectorService.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Service;
 
 import kr.human.anihospital.mapper.PatientInfoEditProtectorMapper;
 import kr.human.anihospital.vo.AnimalVO;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class PatientInfoEditProtectorService {
 
 	@Autowired
 	PatientInfoEditProtectorMapper patientInfoEditProtectorMapper;
-	
+	//----------------------------------------------------------------------------------------------------
 	// 환자 정보 수정 페이지에서 사용할 환자 정보
+	//----------------------------------------------------------------------------------------------------	
 	public AnimalVO selectPatient() {
 		AnimalVO animalVO = new AnimalVO();
 		try {
@@ -25,12 +28,27 @@ public class PatientInfoEditProtectorService {
 		return animalVO;
 	}
 	
+	//----------------------------------------------------------------------------------------------------
 	// 보호자의 환자 정보 수정
+	//----------------------------------------------------------------------------------------------------
 	public void updatePatient(Map<String, Object> map) {
 		try {
 			patientInfoEditProtectorMapper.updatePatient(map);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//----------------------------------------------------------------------------------------------------
+	// 환자 추가 
+	//----------------------------------------------------------------------------------------------------
+	public void insertPatient(AnimalVO animalVO){
+		try {
+			log.info("서비스 받은 값 : {}", animalVO);
+			patientInfoEditProtectorMapper.insertPatient(animalVO);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
